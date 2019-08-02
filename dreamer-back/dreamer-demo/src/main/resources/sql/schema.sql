0ba6bf1366437a119488796f6a313db7434cf8af
tudan110 <wtudan@126.com>
2019-08-02 15:57:27 +0800

    项目启动，自动执行sql，always

--- dreamer-back/dreamer-demo/src/main/resources/sql/schema.sql
+++ dreamer-back/dreamer-demo/src/main/resources/sql/schema.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 01/08/2019 14:12:40
+ Date: 02/08/2019 15:18:27
 */
 
 SET NAMES utf8mb4;
@@ -31,17 +31,5 @@ CREATE TABLE `user`  (
   `update_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = 'user' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('605398903319166976', '土旦', 'wtudan@126.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('605398984650915840', '小蓝鲸', '15952023906@163.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('605399053823377408', 'test', 'test@test.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('605438057910370304', 'asdf', 'a@a.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('605509093947342848', 'update-test', 'test@test.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('606165831289667584', 'insert-test', 'test@test.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('606166383591424000', 'insert-test', 'test@test.com', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES ('606167338433118208', 'insert-test', 'test@test.com', NULL, NULL, NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
